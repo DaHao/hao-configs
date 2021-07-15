15fcb5128b5c2704b302305e1d770b368e3662bb
Hao <jiash561@gmail.com>
2021-07-15 23:01:38 +0800

    feat: update back-vim

--- myvim/backup-vim.sh
+++ myvim/backup-vim.sh
@@ -1,1 +1,14 @@
-cp -R $HOME/.vimrc ./.vimrc
+#!/bin/bash
+
+if [ "$1" = "backup" ]
+then
+    cp -R $HOME/.vimrc ./.vimrc
+    echo File backup
+fi
+
+if [ "$1" = "restore" ]
+then
+    cp -R ./.vimrc $HOME/.vimrc
+    echo File restore
+fi
+
